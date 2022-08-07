d50ab5ba678708bba32eb9635052919dafeea505
Guo Yipeng <yipeng.guo@imperial.ac.uk>
2022-08-07 22:57:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        new MyFrame();
+    }
+}
---
+++ src/sort.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class sort {
+    private JPanel Main;
+    private JButton change;
+    private JButton submit;
+    private JButton clear;
+    private JTable window;
+    private JTextField rootAddress;
+
+    public sort() {
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
